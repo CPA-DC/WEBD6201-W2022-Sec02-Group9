29caac8719e29d9cfc9458e9708013bfe4f0b01f
Katherine Bellman <katherine.bellman@dcmail.ca>
2022-03-31 16:05:21 -0400

    migrated past page scripts to repo

--- Scripts/app.js
+++ Scripts/app.js
@@ -93,22 +93,184 @@
 
     function displayHome()
     {
+        // Name of button and id are same... this is a naming convention. Button is actually an 'html element'
+        console.log("Home Page");
         
+        
+        //Displayed content
+        let AboutUsButton = document.getElementById("AboutUsButton");
+        AboutUsButton.addEventListener("click", function()
+        {
+            location.href = "about.html";
+        });
+
+        // Step 1 - get a reference to an entry point
+        let MainDivContainer = document.getElementsByTagName("main")[0];  
+        let MainDivHeading = document.getElementsByTagName("h1")[0];
+        let HeroImage = document.getElementById("CodeHeroImage");
+
+        // Step 2 - create an html element in memory
+        let HeroParagraph = document.createElement("p");
+
+        // Step 3 - configure new element
+        MainDivContainer.setAttribute("id","MainDivContainer");
+        MainDivHeading.setAttribute("id", "MainDivHeading;");
+        HeroParagraph.setAttribute("id", "HeroParagraph");
+        HeroParagraph.setAttribute("class", "mt-3");
+        HeroParagraph.innerHTML = `This is the landing page for a website in development as part of a ongoing for the course WEBD6201.\nThe creators of this site include Katherine Bellman and Russell Waring, who both both enrolled in\n the Computer Programming and Analysis Program at Durham College. \n We were moulded into developers who put our clients needs first.\n Check out out software development services to get a taste of what we can do for you!\n`;
+
+
+        // Step 4 - perform insertion
+        HeroImage.after(HeroParagraph);
     }
 
     function displayAbout()
     {
 
+       console.log("About Page");
+
+        let Header_About_Header = "About Us";    
+        let About_Cards_Data = [
+            {
+                Name: "Russell Waring",
+                Github: "https://github.com/RussellWaring",
+                blurb:"Greetings. I am a student of Durham College in Oshawa studying Computer Programming and Analysis. I have an Urban Forestry Technician Degree and broad working experience in many fields. Although I grew up in a small town, I have big dreams!",
+                resume_link: "https://www.linkedin.com/in/russell-waring/",
+            },
+            {
+                Name: "Katherine Bellman",
+                Github: "https://github.com/Tsukiyomi-Inari",
+                blurb: "I am Katherine Bellman, studying Computer Programming and Analysis at Durham College in Oshawa. As an Alumni of OCAD University, I apply creative solutions to computational problems and design attractive user interfaces through colour theory practices.",
+                resume_link: "https://dconline-my.sharepoint.com/:b:/g/personal/katherine_bellman_dcmail_ca/EcraWZsK4R5Bo2UXGh91c8MBl_12pDZ66gjbi2QhUquLtg?e=KcHZhB", 
+            },
+        ];
+
+        //Insert the title text
+        let titleH1 = document.getElementsByTagName("h1")[0];
+        titleH1.innerHTML = Header_About_Header;  
+
+        document.getElementsByClassName("card-title")[0].innerHTML = About_Cards_Data[0].Name;
+        //document.getElementsByClassName("card-github")[0].innerHTML = About_Cards_Data[0].Github;
+        document.getElementsByClassName("card-text")[0].innerHTML = About_Cards_Data[0].blurb;
+        let russell_resume = document.getElementsByClassName("card-button")[0]; 
+        russell_resume.setAttribute("href", About_Cards_Data[0].resume_link);
+        russell_resume.textContent = "Resume";
+
+        console.log(russell_resume.getAttribute("href"));
+
+        // Waring resume link
+        let russell_button = document.getElementsByClassName("btn")[0];
+        russell_button.addEventListener("click", function()
+        {
+            location.href = russell_resume.getAttribute("href");
+        });
+
+        document.getElementsByClassName("card-title")[1].innerHTML = About_Cards_Data[1].Name;
+        //document.getElementsByClassName("card-github")[1].innerHTML = About_Cards_Data[1].Github;
+        document.getElementsByClassName("card-text")[1].innerHTML = About_Cards_Data[1].blurb;
+        let bellman_resume = document.getElementsByClassName("card-button")[1]; 
+        bellman_resume.setAttribute("href", About_Cards_Data[0].resume_link);
+        bellman_resume.textContent = "Resume";
+
+        console.log(bellman_resume.getAttribute("href"));
+
+        // Bellman resume link
+        let bellman_button = document.getElementsByClassName("btn")[1];
+        bellman_button.addEventListener("click", function()
+        {
+            location.href = bellman_resume.getAttribute("href");
+        });
+
     }
 
     function displayProjects()
     {
+        console.log("Projects Page");
 
+        let MainDivHeading = document.getElementsByTagName("h1")[0];
+        MainDivHeading.textContent = "Our Projects";
+        //console.log(MainDivHeading);
+
+        // Step 2 - create html element
+        let h6KB = document.getElementsByTagName("h6")[0];
+        let paraKB = document.getElementsByTagName("p")[0];
+
+        let h6RW = document.getElementsByTagName("h6")[1];
+        let paraRW = document.getElementsByTagName("p")[1];
+
+        let h6KB2 = document.getElementsByTagName("h6")[2];
+        let paraKB2 = document.getElementsByTagName("p")[2];
+
+        // Step 3 - configure elements
+        h6KB.innerHTML = "K. Bellman";
+        paraKB.innerHTML = "Cases By Region, NETD2202";
+
+        h6RW.innerHTML = "R. Waring";
+        paraRW.innerHTML = "In-class exercise 4, NETD2202";
+
+        h6KB2.innerHTML = "K. Bellman";
+        paraKB2.innerHTML = "Validation, NETD2202 final";
     }
 
     function displayServices()
     {
+      console.log("Services Page");
+        
+        let Header_Kat_Services = "Katherine Specializes in: ";    
+        let Kat_Services = [
+            {
+                Service: "Web Design",
+                // text:"",
+            },
+            {
+                Service: "Divi Wordpress Development",
+                // text:"",
+            },
+            {
+                Service: "UI Design",
+                // text:"",
+            },
+        ];
 
+
+        let Header_Russel_Services = "Russell Specializes in: ";    
+        let Russel_Services = [
+            {
+                Service: "System Development",
+                //text: "",
+            },
+            {
+                Service: "C# .Net Core Frameworks Development",
+                //text: "",
+            },
+            {
+                Service: "Proofreading Code",
+                //text: "",
+            },
+        ];
+
+        // Insertion point class for each name=> service heading 
+        let serviceHeaderKB = document.getElementsByClassName("service-heading")[0];
+        serviceHeaderKB.innerHTML = Header_Kat_Services;
+        
+        let serviceHeaderRW = document.getElementsByClassName("service-heading")[1];
+        serviceHeaderRW.innerHTML = Header_Russel_Services;  
+
+        
+        // Insertion point class for Name of service => service-note
+        let serviceKB0 = document.getElementsByClassName("service-note")[0];
+        let serviceKB1 = document.getElementsByClassName("service-note")[1];
+        let serviceKB2 = document.getElementsByClassName("service-note")[2];
+        serviceKB0.innerHTML = Kat_Services[0].Service;
+        serviceKB1.innerHTML = Kat_Services[1].Service;
+        serviceKB2.innerHTML = Kat_Services[2].Service;
+
+        let serviceRW0 = document.getElementsByClassName("service-note")[3];
+        let serviceRW1 = document.getElementsByClassName("service-note")[4];
+        let serviceRW2 = document.getElementsByClassName("service-note")[5];
+        serviceRW0.innerHTML = Russel_Services[0].Service;
+        serviceRW1.innerHTML = Russel_Services[1].Service;
+        serviceRW2.innerHTML = Russel_Services[2].Service;
     }
 
     function testFullName()
